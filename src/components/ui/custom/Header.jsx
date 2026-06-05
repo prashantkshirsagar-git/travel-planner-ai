9cb8432330912769303b627dec6b2bf0441370b4
Prashant Kshirsagar <134128560+prashantkshirsagar-git@users.noreply.github.com>
2026-06-05 12:41:16 +0530

    feat: build landing page hero section with heading, copy, and get started button

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "../button";
 function Header() {
   return (
-    <div className="p-2 shadow-sm">
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
       <img src="/logo.svg" />
       <div>
         <Button>Sign In</Button>
--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { Button } from "../button";
 function Hero() {
   return (
-    <div>
-      Hero
+    <div className="flex flex-col items-center mx-56 gap-9">
+      <h1 className='font-extrabold text-[50px] text-center mt-16'>
+        <span className="text-[#f56551]">Discover Your Next Adventure with AI:</span> Personalized Itineraries at Your
+        Fingertips
+      </h1>
+      <p className="text-xl text-gray-500 text-center">
+        Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
+      </p>
+      <Button>
+       Get Started, It's Free 
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
