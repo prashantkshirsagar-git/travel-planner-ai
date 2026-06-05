7989b728f795c59d9cfb13b8d528080fb1f554c7
Prashant Kshirsagar <134128560+prashantkshirsagar-git@users.noreply.github.com>
2026-06-05 09:06:43 +0530

    feat: setup react app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import { Button } from "@/components/ui/button"
+ 
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>I am A black button</Button>
+    </div>
+  )
+}
+ 
+export default App
