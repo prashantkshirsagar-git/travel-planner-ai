76734536517d36c56e064add57c6bdac5d9027b1
Prashant Kshirsagar <134128560+prashantkshirsagar-git@users.noreply.github.com>
2026-06-23 11:31:48 +0530

    feat: show user avatar and My Trips button when signed in

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -1,17 +1,35 @@
 import React, { useEffect } from "react";
 import { Button } from "../button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverDescription,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 function Header() {
-
-   const user= JSON.parse(localStorage.getItem('user'));
-   useEffect(()=>{
-  console.log(user)
-   },[])
+  const user = JSON.parse(localStorage.getItem("user"));
+  useEffect(() => {
+    console.log(user);
+  }, []);
 
   return (
     <div className="p-3 shadow-sm flex justify-between items-center px-5">
       <img src="/logo.svg" />
       <div>
-        {}<Button>Sign In</Button>
+        {user ? (
+          <div className="flex items-center gap-3">
+            <Button variant="outline" className="rounded-full">
+              My Trips
+            </Button>
+
+            <img src={user?.picture} className="h-8.75 w-8.75 rounded-full" />
+            
+          </div>
+        ) : (
+          <Button>Sign In</Button>
+        )}
       </div>
     </div>
   );
