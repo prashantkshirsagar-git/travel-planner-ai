d9edf192e1f0b02da3df581ff70aa5ae1a152c1f
Prashant Kshirsagar <134128560+prashantkshirsagar-git@users.noreply.github.com>
2026-06-23 11:04:56 +0530

    debug: log user from localStorage on Header mount

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "../button";
 function Header() {
+
+   const user= JSON.parse(localStorage.getItem('user'));
+   useEffect(()=>{
+  console.log(user)
+   },[])
+
   return (
     <div className="p-3 shadow-sm flex justify-between items-center px-5">
       <img src="/logo.svg" />
       <div>
-        <Button>Sign In</Button>
+        {}<Button>Sign In</Button>
       </div>
     </div>
   );
